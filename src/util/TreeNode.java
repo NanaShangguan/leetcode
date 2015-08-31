1a0ac8e97bfe2622e94d0bf108310949490c24b4
NanoShangguan <nano.shangguan@icloud.com>
2015-08-31 16:40:48 +0800

    2015-08-31 16:40

---
+++ src/util/TreeNode.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Created by Nano on 2015/8/31.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
