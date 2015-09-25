aa016199cb17f045faedc649d8564b46c8eeda11
NanoShangguan <nano.shangguan@icloud.com>
2015-09-25 13:53:27 +0800

    2015-09-25 13:53

--- src/nano/shangguan/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/nano/shangguan/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -8,6 +8,27 @@ import util.TreeNode;
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+        return buildBinaryTree(preorder, 0, preorder.length - 1
+                , inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode buildBinaryTree(int[] preorder, int preStart, int preEnd
+            , int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd) return null;
+        int rootValue = preorder[preStart];
+        TreeNode root = new TreeNode(rootValue);
+        int leftOrderLength = 0;
+        while (inorder[inStart + leftOrderLength] != rootValue) { leftOrderLength++; }
+        root.left = buildBinaryTree(preorder, preStart + 1, preStart + leftOrderLength
+                , inorder, inStart, inStart + leftOrderLength - 1);
+        root.right = buildBinaryTree(preorder, preStart + leftOrderLength + 1, preEnd
+                , inorder, inStart + leftOrderLength + 1, inEnd);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ConstructBinaryTreeFromPreorderAndInorderTraversal()
+                .buildTree(new int[]{ 1 }
+                        , new int[]{ 1 }));
     }
 }
