885d9cd292e5d6f71a04940433d8c15088792aaa
NanoShangguan <nano.shangguan@icloud.com>
2015-10-12 20:24:37 +0800

    2015-10-12 20:24

--- src/nano/shangguan/SearchA2DMatrixII.java
+++ src/nano/shangguan/SearchA2DMatrixII.java
@@ -5,53 +5,23 @@ package nano.shangguan;
  */
 public class SearchA2DMatrixII {
     /**
-     * 蛇形搜索
+     * 经典题，从右上角开始搜索，没用到二分搜索
+     * 时间复杂度O(m + n)
+     * 印象笔记中记录了其他解法
      * @param matrix
      * @param target
      * @return
      */
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row_low = 0, row_high = matrix.length - 1;
-        while (row_low <= row_high) {
-            int row_middle = (row_low + row_high) / 2;
-            int[] row = matrix[row_middle];
-            int col_low = 0, col_high = row.length - 1;
-            while (col_low <= col_high) {
-                int col_middle = (col_low + col_high) / 2;
-                int value = row[col_middle];
-                if (value == target) return true;
-                else if (value < target) col_low = col_middle + 1;
-                else col_high = col_middle - 1;
-            }
-
-            if ((col_low + col_high) / 2 >= (row.length - 1) / 2)
-                row_low = row_middle + 1;
-            else row_high = row_middle - 1;
+        int row_num = matrix.length, col_num = matrix[0].length;
+        int i = 0, j = matrix[0].length - 1;
+        while (i < row_num && i > -1 && j < col_num && j > -1) {
+            int value = matrix[i][j];
+            if (value == target) return true;
+            else if (value < target) i++;
+            else j--;
         }
         return false;
-    }
-
-    /**
-     * 在矩阵的第row行的[low, high]中寻找最后一个比target小的元素的index
-     * @param matrix 矩阵
-     * @param rowIndex 行号
-     * @param low 起始位置
-     * @param high 结束位置
-     * @param target 目标值
-     * @return
-     */
-    public int searchInRow(int[][] matrix, int rowIndex, int low, int high, int target) {
-        int[] row = matrix[rowIndex];
-        while (low <= high) {
-            int middle = (low + high) / 2;
-            int value = row[middle];
-            if (value == target) return middle;
-            else if (value < target) low = middle + 1;
-            else high = middle - 1;
-        }
-        if (high == -1) return -1;
-        else if (low == row.length) return row.length;
-        else return (row[low] > row[high] ? high : low);
     }
 
     public static void main(String[] args) {
@@ -62,6 +32,6 @@ public class SearchA2DMatrixII {
                 , {10, 13, 14, 24, 25}
                 , {18, 21, 23, 26, 30}
         };
-        System.out.println(new SearchA2DMatrixII().searchInRow(matrix, 2, 0, 4, 21));
+        System.out.println(new SearchA2DMatrixII().searchMatrix(matrix, 20));
     }
 }
