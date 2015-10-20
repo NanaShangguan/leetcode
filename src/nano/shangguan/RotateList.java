87799d40994389def065fda8357d03f9fc915b70
NanoShangguan <nano.shangguan@icloud.com>
2015-10-20 11:08:11 +0800

    2015-10-20 11:07

--- src/nano/shangguan/RotateList.java
+++ src/nano/shangguan/RotateList.java
@@ -8,18 +8,28 @@ import util.ListNode;
 public class RotateList {
     public ListNode rotateRight(ListNode head, int k) {
         if (head != null) {
-            ListNode fast = head, slow = null;
+            //获得链表长度
+            int len = 0;
+            ListNode node = head;
+            while (node != null) {
+                len++;
+                node = node.next;
+            }
+            k = k % len;
+            //寻找pivot的prev位置
+            ListNode fast = head, slow = null, prev = null;
             k = -k;
-            while (fast.next != null) {
-                k++;
+            while (fast != null) {
                 if (k >= 0) {
                     if (slow == null) slow = head;
                     else slow = slow.next;
                 }
+                prev = fast;
                 fast = fast.next;
+                k++;
             }
-            if (slow != null) {
-                fast.next = head;
+            if (slow != null && slow != prev) {
+                prev.next = head;
                 head = slow.next;
                 slow.next = null;
             }
