ffddbf0e6ca4d547492b4174a74a0413197de6b0
NanoShangguan <nano.shangguan@icloud.com>
2015-10-21 09:49:25 +0800

    2015-10-21 9:49

--- src/util/ListNode.java
+++ src/util/ListNode.java
@@ -7,4 +7,15 @@ public class ListNode {
     public int val;
     public ListNode next;
     public ListNode(int x) { val = x; }
+
+    @Override
+    public String toString() {
+        String str = val + "";
+        ListNode node = this.next;
+        while (node != null) {
+            str += "->" + node.val;
+            node = node.next;
+        }
+        return str;
+    }
 }
