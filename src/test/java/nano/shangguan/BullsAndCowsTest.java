944fa285a530ffcd97893e45785e38b0da653144
NanoShangguan <nano.shangguan@icloud.com>
2015-11-03 11:30:37 +0800

    2015-11-03 11:30

---
+++ src/test/java/nano/shangguan/BullsAndCowsTest.java
@@ -0,0 +1,18 @@
+package nano.shangguan;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nano on 2015/11/3.
+ */
+public class BullsAndCowsTest {
+
+    @Test
+    public void testGetHint() throws Exception {
+        String result = new BullsAndCows().getHint("11", "10");
+        Assert.assertEquals("结果错误", result, "1A0B");
+    }
+}
