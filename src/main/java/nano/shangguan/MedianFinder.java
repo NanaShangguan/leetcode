7b76e2a9e84cc0cf964d29949dbf8e63f223e7e7
NanoShangguan <nano.shangguan@icloud.com>
2015-11-24 12:44:35 +0800

    2015-11-24 12:44

--- src/main/java/nano/shangguan/MedianFinder.java
+++ src/main/java/nano/shangguan/MedianFinder.java
@@ -1,41 +1,59 @@
 package nano.shangguan;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Created by Nano on 2015/11/23.
- * 插入排序的思想
+ * 两个优先队列，一个大根堆，一个小根堆
+ * 大根堆保存前一半元素，小根堆保存后一半元素
  */
 public class MedianFinder {
-    private List<Integer> array = new ArrayList<Integer>();
+    private PriorityQueue<Integer> queueMax = new PriorityQueue<Integer>(new Comparator<Integer>() {
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    });
+    private PriorityQueue<Integer> queueMin = new PriorityQueue<Integer>();
     // Adds a number into the data structure.
     public void addNum(int num) {
-        int rightPlace = findPlace(num);
-        array.add(rightPlace, num);
+        int leftSize = queueMax.size();
+        int rightSize = queueMin.size();
+        if (leftSize + rightSize == 0) queueMax.add(num);
+        else if (leftSize == 1 && rightSize == 0) {
+            //初始化
+            if (queueMax.peek() >= num) {
+                queueMin.add(queueMax.poll());
+                queueMax.add(num);
+            } else queueMin.add(num);
+        } else {
+            int max = queueMax.peek();
+            int min = queueMin.peek();
+            if (leftSize == rightSize) {
+                if (num < min) queueMax.add(num);
+                else {
+                    queueMax.add(queueMin.poll());
+                    queueMin.add(num);
+                }
+            } else {
+                if (num > max) queueMin.add(num);
+                else {
+                    queueMin.add(queueMax.poll());
+                    queueMax.add(num);
+                }
+            }
+        }
     }
 
     // Returns the median of current data stream
     public double findMedian() {
-        if (array.size() % 2 == 1) {
-            return array.get(array.size() / 2);
+        int sumCount = queueMax.size() + queueMin.size();
+        if (sumCount % 2 == 1) {
+            return queueMax.peek();
         } else {
-            int middleIndex = array.size() / 2;
-            int middle1 = array.get(middleIndex - 1);
-            int middle2 = array.get(middleIndex);
+            int middle1 = queueMax.peek();
+            int middle2 = queueMin.peek();
             return (middle1 + middle2) / 2.0;
         }
-    }
-
-    private int findPlace(int num) {
-        int low = 0, high = array.size() - 1;
-        while (low <= high) {
-            int middle = (low + high) / 2;
-            int middleValue = array.get(middle);
-            if (middleValue == num) return middle;
-            else if (middleValue < num) low = middle + 1;
-            else high = middle - 1;
-        }
-        return low;
     }
 }
--- src/test/java/nano/shangguan/MedianFinderTest.java
+++ src/test/java/nano/shangguan/MedianFinderTest.java
@@ -9,11 +9,13 @@ public class MedianFinderTest extends TestCase {
 
     public void testFindMedian() throws Exception {
         MedianFinder finder = new MedianFinder();
-        finder.addNum(3);
-        assertEquals("Wrong Answer", 3.0, finder.findMedian());
-        finder.addNum(1);
-        assertEquals("Wrong Answer", 2.0, finder.findMedian());
-        finder.addNum(2);
-        assertEquals("Wrong Answer", 2.0, finder.findMedian());
+        finder.addNum(-1);
+//        assertEquals("Wrong Answer", 3.0, finder.findMedian());
+        finder.addNum(-2);
+//        assertEquals("Wrong Answer", 2.0, finder.findMedian());
+        finder.addNum(-3);
+        finder.addNum(-4);
+        finder.addNum(-5);
+        assertEquals("Wrong Answer", -3.0, finder.findMedian());
     }
 }
