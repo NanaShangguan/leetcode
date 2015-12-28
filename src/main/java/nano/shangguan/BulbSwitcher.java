b54538e6b262ac915392a403709de21aa3271007
NanoShangguan <nano.shangguan@icloud.com>
2015-12-28 16:04:44 +0800

    2015-12-28 16:04

---
+++ src/main/java/nano/shangguan/BulbSwitcher.java
@@ -0,0 +1,10 @@
+package main.java.nano.shangguan;
+
+/**
+ * Created by Nano on 2015/12/28.
+ */
+public class BulbSwitcher {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
