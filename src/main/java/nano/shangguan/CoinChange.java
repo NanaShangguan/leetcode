ed3ca1619d44fb1cbbfe99456d87cd1efaf596d5
NanoShangguan <nano.shangguan@icloud.com>
2016-01-04 15:09:37 +0800

    2016-01-04 15:09

--- src/main/java/nano/shangguan/CoinChange.java
+++ src/main/java/nano/shangguan/CoinChange.java
@@ -1,31 +1,29 @@
 package nano.shangguan;
 
-import java.util.Arrays;
 
 /**
  * Created by Nano on 2015/12/28.
  */
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
-        //先对coins数组排序
-        Arrays.sort(coins);
-        //回溯法
-        return helper(coins, coins.length - 1, amount);
-    }
+        if (amount <= 0) return 0;
+        int[] dp = new int[amount + 1];
 
-    private int helper(int[] coins, int end, int amount) {
-        int maxCoin = coins[end];
-        if (amount % maxCoin == 0) return amount / maxCoin;
-        if (end == 0) return -1;
-        int maxMultiples = amount / maxCoin;
-        int min = -1;
-        for (int i = maxMultiples; i >= 0; i--) {
-            int result = helper(coins, end - 1, amount - i * maxCoin);
-            if (result != -1) {
-                int currentNum = result + i;
-                if (min == -1 || min > currentNum) min = currentNum;
+        for (int i = 1; i <= amount; i++) {
+            if (dp[i] != 1) {
+                int min = -1;
+                for (int coin : coins) {
+                    if (i == coin) {
+                        min = 1;
+                        break;
+                    } else if (i > coin && dp[i - coin] != -1) {
+                        int count = dp[i - coin] + 1;
+                        if (min == -1 || min > count) min = count;
+                    }
+                }
+                dp[i] = min;
             }
         }
-        return min;
+        return dp[amount];
     }
 }
--- src/test/java/nano/shangguan/CoinChangeTest.java
+++ src/test/java/nano/shangguan/CoinChangeTest.java
@@ -8,9 +8,10 @@ import junit.framework.TestCase;
 public class CoinChangeTest extends TestCase {
 
     public void testCoinChange() throws Exception {
-//        assertEquals("Wrong Answer", 3, new CoinChange().coinChange(new int[]{1, 2, 5}, 11));
-//        assertEquals("Wrong Answer", -1, new CoinChange().coinChange(new int[]{2}, 3));
-//        assertEquals("Wrong Answer", 6, new CoinChange().coinChange(new int[]{2, 4, 5}, 26));
-        assertEquals("Wrong Answer", 20, new CoinChange().coinChange(new int[]{186, 419, 83, 408}, 6249));
+        CoinChange coinChange = new CoinChange();
+        assertEquals("Wrong Answer", 3, coinChange.coinChange(new int[]{1, 2, 5}, 11));
+        assertEquals("Wrong Answer", -1, coinChange.coinChange(new int[]{2}, 3));
+        assertEquals("Wrong Answer", 6, coinChange.coinChange(new int[]{2, 4, 5}, 26));
+        assertEquals("Wrong Answer", 20, coinChange.coinChange(new int[]{186, 419, 83, 408}, 6249));
     }
 }
